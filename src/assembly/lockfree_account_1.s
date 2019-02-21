73a20fd73e0bd1d68d60d0680734dfcc4981e08a
Jordan Mackie <jmackie97@hotmail.com>
2019-02-21 19:09:04 +0000

    Add assembly for lockfree and different optimisation levels

---
+++ src/assembly/lockfree_account_1.s
@@ -0,0 +1,78 @@
+	.file	"lockfree_account.c"
+	.text
+	.globl	createAccount
+	.type	createAccount, @function
+createAccount:
+.LFB19:
+	.cfi_startproc
+	pushq	%rbx
+	.cfi_def_cfa_offset 16
+	.cfi_offset 3, -16
+	movl	%edi, %ebx
+	movl	$4, %edi
+	call	malloc@PLT
+	movl	%ebx, (%rax)
+	popq	%rbx
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE19:
+	.size	createAccount, .-createAccount
+	.globl	getBalance
+	.type	getBalance, @function
+getBalance:
+.LFB20:
+	.cfi_startproc
+	movl	(%rdi), %eax
+	ret
+	.cfi_endproc
+.LFE20:
+	.size	getBalance, .-getBalance
+	.globl	deposit
+	.type	deposit, @function
+deposit:
+.LFB21:
+	.cfi_startproc
+	movq	%rdi, %rdx
+.L5:
+	movl	(%rdi), %eax
+	leal	(%rax,%rsi), %ecx
+	lock cmpxchgl	%ecx, (%rdx)
+	jne	.L5
+	rep ret
+	.cfi_endproc
+.LFE21:
+	.size	deposit, .-deposit
+	.globl	withdraw
+	.type	withdraw, @function
+withdraw:
+.LFB22:
+	.cfi_startproc
+	movq	%rdi, %rdx
+.L8:
+	movl	(%rdi), %eax
+	movl	%eax, %ecx
+	subl	%esi, %ecx
+	lock cmpxchgl	%ecx, (%rdx)
+	jne	.L8
+	movl	%esi, %eax
+	ret
+	.cfi_endproc
+.LFE22:
+	.size	withdraw, .-withdraw
+	.globl	destroyAccount
+	.type	destroyAccount, @function
+destroyAccount:
+.LFB23:
+	.cfi_startproc
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 16
+	call	free@PLT
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE23:
+	.size	destroyAccount, .-destroyAccount
+	.ident	"GCC: (Ubuntu 7.3.0-27ubuntu1~18.04) 7.3.0"
+	.section	.note.GNU-stack,"",@progbits
